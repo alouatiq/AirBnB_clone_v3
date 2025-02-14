5abf5e3c325c636b8e88255bd3abb3b0f79f172d
AL H. <alh@dr.com>
2025-02-15 00:57:38 +0100

    Task 6: Implement RESTful actions for State objects

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,4 +6,8 @@ from flask import Blueprint  # Moved import to top
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 # Optionally import the routes after app_views is created:
+# Import index routes
 from api.v1.views.index import *  # Keep wildcard import if required
+
+# Import states routes
+from api.v1.views.states import *
