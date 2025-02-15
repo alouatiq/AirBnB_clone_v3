20805ca4ba80fb835642c1c1763c42dfe2609e96
Anas2018EMI <anassaatef@gmail.com>
2025-02-15 20:24:07 +0000

    Initial commit

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,69 +1,73 @@
 #!/usr/bin/python3
-"""State objects that handles all default RESTFul API actions"""
-
+""" View for User objects that handles default API actions """
 from api.v1.views import app_views
+from flask import jsonify, abort, make_response, request
 from models import storage
 from models.user import User
-from flask import abort, request, jsonify
+import hashlib
 
 
-@app_views.route("/users", strict_slashes=False, methods=["GET"])
-@app_views.route("/users/<user_id>", strict_slashes=False,
-                 methods=["GET"])
-def user(user_id=None):
-    """show user and user with id"""
-    user_list = []
-    if user_id is None:
-        all_objs = storage.all(User).values()
-        for v in all_objs:
-            user_list.append(v.to_dict())
-        return jsonify(user_list)
-    else:
-        result = storage.get(User, user_id)
-        if result is None:
-            abort(404)
-        return jsonify(result.to_dict())
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def users():
+    """ Retrieves the list of all User objects """
+    d_users = storage.all(User)
+    return jsonify([obj.to_dict() for obj in d_users.values()])
 
 
-@app_views.route("/users/<user_id>", strict_slashes=False,
-                 methods=["DELETE"])
-def user_delete(user_id):
-    """delete method"""
-    obj = storage.get(User, user_id)
-    if obj is None:
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def r_user_id(user_id):
+    """
+    file: yml/users_get.yml
+    """
+    user = storage.get("User", user_id)
+    if not user:
         abort(404)
-    storage.delete(obj)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def del_user(user_id):
+    """ Deletes a User object """
+    user = storage.get("User", user_id)
+    if not user:
+        abort(404)
+    user.delete()
     storage.save()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route("/users", strict_slashes=False, methods=["POST"])
-def create_user():
-    """create a new post req"""
-    data = request.get_json(force=True, silent=True)
-    if not data:
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def post_user():
+    """ Creates a User object """
+    new_user = request.get_json()
+    if not new_user:
         abort(400, "Not a JSON")
-    if "email" not in data:
+    if "email" not in new_user:
         abort(400, "Missing email")
-    if "password" not in data:
+    if "password" not in new_user:
         abort(400, "Missing password")
-    new_user = User(**data)
-    new_user.save()
-    return jsonify(new_user.to_dict()), 201
+
+    user = User(**new_user)
+    storage.new(user)
+    storage.save()
+    return make_response(jsonify(user.to_dict()), 201)
 
 
-@app_views.route("/users/<user_id>", strict_slashes=False,
-                 methods=["PUT"])
-def update_user(user_id):
-    """update user"""
-    obj = storage.get(User, user_id)
-    if obj is None:
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def put_user(user_id):
+    """ Updates a User object """
+    user = storage.get("User", user_id)
+    if not user:
         abort(404)
-    data = request.get_json(force=True, silent=True)
-    if not data:
+
+    body_request = request.get_json()
+    if not body_request:
         abort(400, "Not a JSON")
-    obj.password = data.get("password", obj.password)
-    obj.first_name = data.get("first_name", obj.first_name)
-    obj.last_name = data.get("last_name", obj.last_name)
-    obj.save()
-    return jsonify(obj.to_dict()), 200
+
+    for k, v in body_request.items():
+        if k not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, k, v)
+
+    storage.save()
+    return make_response(jsonify(user.to_dict()), 200)
