705435a6262d44ac23b0f9fcd628756126456701
Anas2018EMI <anassaatef@gmail.com>
2025-02-15 13:32:38 +0000

    Initial commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """Initialize Blueprint for views"""
-
 from flask import Blueprint  # Moved import to top
+from api.v1.views.cities import *
+from api.v1.views.states import *
+from api.v1.views.index import *  # Keep wildcard import if required
+
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 # Optionally import the routes after app_views is created:
 # Import index routes
-from api.v1.views.index import *  # Keep wildcard import if required
 
 # Import states routes
-from api.v1.views.states import *
 # Import cities routes
-from api.v1.views.cities import *
